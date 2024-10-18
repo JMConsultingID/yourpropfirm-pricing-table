1b51616f94dd913dfcb01a295b1348eb488aec98
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-10-18 15:59:43 +0800

    Update yourpropfirm-pricing-table.php

--- yourpropfirm-pricing-table.php
+++ yourpropfirm-pricing-table.php
@@ -44,14 +44,14 @@ function yourpropfirm_pricing_table_pricing_table() {
         wp_enqueue_style( 'yourpropfirm-pricing-table-swiper-bundle-css', $plugin_url . '/assets/css/swiper-bundle.min.css');
         wp_enqueue_style( 'yourpropfirm-pricing-table-tippy-css', $plugin_url . '/assets/css/tippy.css');
         wp_enqueue_style( 'yourpropfirm-pricing-table-tippy-light-css', $plugin_url . '/assets/css/tippy-light.css');
-        wp_enqueue_style( 'yourpropfirm-pricing-table-plugins-css', $plugin_url . '/assets/css/yourpropfirm-pricing-table-pricing-table.css', array('yourpropfirm-pricing-table-font-awesome-css', 'yourpropfirm-pricing-table-swiper-bundle-css', 'yourpropfirm-pricing-table-tippy-css', 'yourpropfirm-pricing-table-tippy-light-css'), YOURPROPFIRM_TABLE_VERSION, 'all' );
+        wp_enqueue_style( 'yourpropfirm-pricing-table-plugins-css', $plugin_url . '/assets/css/yourpropfirm-pricing-table-public.css', array('yourpropfirm-pricing-table-font-awesome-css', 'yourpropfirm-pricing-table-swiper-bundle-css', 'yourpropfirm-pricing-table-tippy-css', 'yourpropfirm-pricing-table-tippy-light-css'), YOURPROPFIRM_TABLE_VERSION, 'all' );
 
         // Enqueue scripts        
         wp_enqueue_script( 'yourpropfirm-pricing-table-swiper-bundle-js', $plugin_url . '/assets/js/swiper-bundle.min.js', array('jquery'), null, true );
         wp_enqueue_script( 'yourpropfirm-pricing-table-popper-js', $plugin_url . '/assets/js/popper.min.js', array(), null, true );
         wp_enqueue_script( 'yourpropfirm-pricing-table-tippy-js', $plugin_url . '/assets/js/tippy-bundle.umd.min.js', array(), null, true );
 
-        wp_enqueue_script( 'yourpropfirm-pricing-table-pricing-table-level-all-js', $plugin_url . '/assets/js/yourpropfirm-pricing-table-pricing-table-level-all.js', array('jquery', 'yourpropfirm-pricing-table-swiper-bundle-js','yourpropfirm-pricing-table-popper-js', 'yourpropfirm-pricing-table-tippy-js'), YOURPROPFIRM_TABLE_VERSION, true );
+        wp_enqueue_script( 'yourpropfirm-pricing-table-pricing-table-level-all-js', $plugin_url . '/assets/js/yourpropfirm-pricing-table-public.js', array('jquery', 'yourpropfirm-pricing-table-swiper-bundle-js','yourpropfirm-pricing-table-popper-js', 'yourpropfirm-pricing-table-tippy-js'), YOURPROPFIRM_TABLE_VERSION, true );
     }
 }
 add_action( 'wp_enqueue_scripts', 'yourpropfirm_pricing_table_pricing_table', 20);
