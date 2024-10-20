9870edcb0777d18ef20544e20345c13976c472de
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-10-20 20:49:31 +0800

    Update yourpropfirm-pricing-table-public.js

--- assets/js/yourpropfirm-pricing-table-public.js
+++ assets/js/yourpropfirm-pricing-table-public.js
@@ -175,9 +175,6 @@
             activeTabContentLevel3.swiperInstance = initTabSwiper(activeTabContentLevel3);
             activeTabContentLevel3.swiperInstance.slideTo(activeSlideIndex, 0);
         }
-
-        // Initialize sub-tabs on initial load
-        initSubTabs(activeTabContent); // Ensure sub-tabs are initialized for the initially active tab
         activateFirstSubTab(activeTabContent); // Auto activate first sub-tab on load
     }
 
